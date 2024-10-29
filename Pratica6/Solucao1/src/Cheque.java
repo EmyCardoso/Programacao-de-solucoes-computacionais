c84f564357aa9e946204859e7ca7a7ba54107bd9
Cardoso Emilyn <127335109+EmyCardoso@users.noreply.github.com>
2024-10-28 23:23:28 -0300

    Add files via upload

---
+++ Pratica6/Solucao1/src/Cheque.java
@@ -0,0 +1,19 @@
+
+public class Cheque {
+
+	private String numeroDoCheque;
+
+    public Cheque (String nomeDoPagador, String cpf, double valorASerPago, String numeroDoCheque){
+        super(nomeDoPagador, cpf, valorASerPago);
+        this.numeroDoCheque = numeroDoCheque;
+    }
+
+    public String getNumeroDoCheque(){
+        return numeroDoCheque;
+    }
+
+    public void setNumeroDoCheque(String numeroDoCheque){
+        this.numeroDoCheque = numeroDoCheque;
+    }
+
+}
