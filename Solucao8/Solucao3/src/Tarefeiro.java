516e2696d4ae2a343308d47dded265307fec1549
Cardoso Emilyn <127335109+EmyCardoso@users.noreply.github.com>
2024-11-16 19:27:19 -0300

    Add files via upload

---
+++ Solucao8/Solucao3/src/Tarefeiro.java
@@ -0,0 +1,19 @@
+
+public class Tarefeiro extends Empregado {
+
+	 private double salario;
+
+	    public Tarefeiro(String nome, double salario){
+	        super(nome, salario);
+	        this.salario = salario;
+	    }
+
+	    public double salario (){
+	        return this.salario;
+	    }
+
+	    public String dados(){
+	        return super.getNome();
+	    }
+	}
+
