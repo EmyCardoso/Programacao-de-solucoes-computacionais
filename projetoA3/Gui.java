504e60e2f9dc344bb84eb0888f04c389e9029d57
EmyCardoso <emimi_utgi@icloud.com>
2024-12-05 18:03:42 -0300

    projeto A3

---
+++ projetoA3/Gui.java
@@ -0,0 +1,24 @@
+// import  javax.swing.*;
+import javax.swing.JOptionPane;
+
+
+public class Gui
+{   private final String sOptions[];
+    
+    public Gui(String sG[])
+    {   sOptions = sG;
+    }
+    
+    public  int getOption()
+    {   return  JOptionPane.showOptionDialog
+                (   null, 
+                    "Digite a opção desejada:", 
+                    "Conexão do Java com o MySQL",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE,
+                    null,
+                    sOptions,
+                    sOptions[sOptions.length-1]
+                );
+    }
+}
